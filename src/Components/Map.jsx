451bebadd2642b5f4ae78f00c5a86f86f474d776
armandocano91 <armandocano.ac@gmail.com>
2018-04-18 13:05:08 -0500

    Google maps can show entries per request

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   withScriptjs,
   withGoogleMap,
@@ -7,106 +7,134 @@ import {
   google,
   maps
 } from "react-google-maps";
-
 import {compose, withProps, lifecycle} from 'recompose';
 import _ from 'lodash'
 import SearchBox from "react-google-maps/lib/components/places/SearchBox";
+import TableList from "./TableList.jsx";
+import {Grid, Row, Col} from 'react-bootstrap';
+import '../App.css'
 
+const Map = compose(withProps({
+  googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBD2x3uoBXe8vWtIaBkvRvlrD9msa-X" +
+      "hnM&v=3.exp&libraries=geometry,drawing,places",
+  loadingElement: <div style={{
+    height: `300%`
+  }}/>,
+  containerElement: <div style={{
+    height: `400px`
+  }}/>,
+  mapElement: <div style={{
+      height: `90%`,
+      width: `100%`
+    }}/>
+}), lifecycle({
+  componentWillMount() {
+    const refs = {};
 
-const Map = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBD2x3uoBXe8vWtIaBkvRvlrD9msa-XhnM&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `50%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `90%`, width:`50%` }} />
-    }),
-  lifecycle({
-    componentWillMount(){
-      const refs = {};
+    this.setState({
+      bounds: null,
+      places:[],
+      center: {
+        lat: 19.39776549999999,
+        lng: -99.1713954,
+        //  results: window.googleMapsData
+      },
+      markers: [],
+      onMapMounted: ref => {
+        refs.map = ref;
+      },
+      onBoundsChanged: () => {
+        this.setState({
+          bounds: refs
+            .map
+            .getBounds(),
+          center: refs
+            .map
+            .getCenter()
+        })
+      },
+      onSearchBoxMounted: ref => {
+        refs.searchBox = ref;
+      },
+      onPlacesChanged: () => {
+        const places = refs
+          .searchBox
+          .getPlaces();
+        const bounds = new window
+          .google
+          .maps
+          .LatLngBounds({
+            lat: 19.308451,
+            lng: -99.232083
+          }, {
+            lat: 19.567233,
+            lng: -99.016398
+          });
 
-      this.setState({
-         bounds: null,
-         center: {
-           lat: 19.39776549999999, lng: -99.1713954
-         },
-         markers: [],
-         onMapMounted: ref => {
-           refs.map = ref;
-         },
-         onBoundsChanged: () => {
-           this.setState({
-             bounds: refs.map.getBounds(),
-             center: refs.map.getCenter(),
-           })
-         },
-         onSearchBoxMounted: ref => {
-           refs.searchBox = ref;
-         },
-         onPlacesChanged: () => {
-           const places = refs.searchBox.getPlaces();
-           const bounds = new window.google.maps.LatLngBounds({lat:19.308451, lng:-99.232083},{lat: 19.567233, lng:-99.016398});
+        places.forEach(place => {
+          if (place.geometry.viewport) {
+            bounds.union(place.geometry.viewport)
+          } else {
+            bounds.extend(place.geometry.location)
+          }
+        });
+        const nextMarkers = places.map(place => ({position: place.geometry.location}));
+        const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
 
-           places.forEach(place => {
-             if (place.geometry.viewport) {
-               bounds.union(place.geometry.viewport)
-             } else {
-               bounds.extend(place.geometry.location)
-             }
-           });
-           const nextMarkers = places.map(place => ({
-             position: place.geometry.location,
-           }));
-           const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
-
-           this.setState({
-             center: nextCenter,
-             markers: nextMarkers,
-           });
-           // refs.map.fitBounds(bounds);
-         }
-       })
-    }
-  }),
-  withScriptjs,
-  withGoogleMap
-)
-(props =>
-  <GoogleMap
-    ref={props.onMapMounted}
-    defaultZoom={15}
-    center={props.center}
-    onBoundsChanged={props.onBoundsChanged}
-  >
-    <SearchBox
-      ref={props.onSearchBoxMounted}
-      bounds={props.bounds}
-      controlPosition={1}
-      onPlacesChanged={props.onPlacesChanged}
-    >
-    <input
-        type="text"
-        placeholder="Customized your placeholder"
-        style={{
-          boxSizing: `border-box`,
-          border: `1px solid transparent`,
-          width: `240px`,
-          height: `32px`,
-          marginTop: `27px`,
-          padding: `0 12px`,
-          borderRadius: `3px`,
-          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-          fontSize: `14px`,
-          outline: `none`,
-          textOverflow: `ellipses`,
-        }}
-      />
-    </SearchBox>
-    {props.markers.map((marker, index) =>
-      <Marker key={index} position={marker.position} />
+        this.setState({center: nextCenter, markers: nextMarkers, places});
+        // refs.map.fitBounds(bounds);
+      }
+    })
+  }
+}), withScriptjs, withGoogleMap)(props => 
+<Grid>
+  <Row className="show-grid">
+    <Col md={6} mdPush={6}>
+      <GoogleMap
+        ref={props.onMapMounted}
+        defaultZoom={15}
+        center={props.center}
+        onBoundsChanged={props.onBoundsChanged}>
+        <SearchBox
+          ref={props.onSearchBoxMounted}
+          bounds={props.bounds}
+          controlPosition={1}
+          onPlacesChanged={props.onPlacesChanged}>
+          <input
+            type="text"
+            placeholder="Hungry?"
+            style={{
+            boxSizing: `border-box`,
+            border: `3px solid transparent`,
+            width: `200px`,
+            height: `32px`,
+            marginTop: `8px`,
+            padding: `0 62px`,
+            borderRadius: `31px`,
+            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+            fontSize: `16px`,
+            outline: `none`,
+            textOverflow: `ellipses`,
+            color: `#f87800`,
+          }}/>
+        </SearchBox>
+        {props
+          .markers
+          .map((marker, index) => <Marker key={index} position={marker.position}/>)}
+      </GoogleMap>
+    </Col>
+    <Col md={6} mdPull={6}>
+      {props.places.map(({place_id, formatted_address, rating, vicinity, name})=>
+          <div>
+          {name}
+          {formatted_address}
+          {rating}
+          {vicinity}
+          </div>
+          
     )}
-  </GoogleMap>
-);
-
-
+    </Col>
+  </Row>
+</Grid>);
 
 export default Map;
--- src/Components/TableList.jsx
+++ src/Components/TableList.jsx
@@ -1,50 +1,12 @@
 import React, {Component} from 'react';
+import Map from './Map.jsx'
+// import Map from './TableListEntry';
 
-class TableList extends Component {
-    render(){
-        return(
-            <div>
-                  <div className="tableList-entry list-group">
-                    <div className="media-left media-middle">
-                      <img className="media-object" />
-                    </div>
-                  <div className="list-body">
-                       <h1>this component is rendering!</h1>
-                      {/* <div className="result-list-entry-title" onClick={() =>{
-                      handleVideolistEntryTitleClick(result)}}>{result.businesses.name}
-                     </div> */}
-                    <div className="result-list-entry-detail"></div>
-                   </div>
-                  </div>
-                  <h1></h1>
-                  <div className="tableList-entry list-group">
-                  <div className="media-left media-middle">
-                    <img className="media-object" />
-                  </div>
-                  <div className="list-body">
-                  <h1>this component is rendering!</h1>
-                    {/* <div className="result-list-entry-title" onClick={() =>{
-                     handleVideolistEntryTitleClick(result)}}>{result.businesses.name}
-                  </div> */}
-                    <div className="result-list-entry-detail"></div>
-                  </div>
-                  </div>
-                  <h1></h1>
-                  <div className="tableList-entry list-group">
-                  <div className="media-left media-middle">
-                    <img className="media-object" />
-                  </div>
-                  <div className="list-body">
-                  <h1>this component is rendering!</h1>
-                    {/* <div className="result-list-entry-title" onClick={() =>{
-                     handleVideolistEntryTitleClick(result)}}>{result.businesses.name}
-                  </div> */}
-                    <div className="result-list-entry-detail"></div>
-                  </div>
-                  </div>
-                  </div>
-        )};
-}
+const TableList = (props) =>(
+          <div className="tablelist">
+            </div>
+);
+
 export default TableList;
 
 
